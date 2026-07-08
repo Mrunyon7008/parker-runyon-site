3dcea8288fe6274f251bccb5c55fd17efca33f51
Myles Runyon <mylesrunyon@Myless-MacBook-Pro.local>
2026-07-08 10:09:46 -0500

    Fix LinkedIn button to link to actual profile

--- app/page.tsx
+++ app/page.tsx
@@ -296,7 +296,9 @@ export default function Home() {
                 Email Me
               </a>
               <a
-                href="https://linkedin.com/in/REPLACE-ME"
+                href="https://www.linkedin.com/in/myles-runyon/"
+                target="_blank"
+                rel="noreferrer"
                 className="btn btn-ghost"
               >
                 LinkedIn
